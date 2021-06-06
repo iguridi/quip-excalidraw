fa4e77d1dc3eb566494a541e12a526abd57f441e
Ignacio Guridi <iguridi@fb.com>
2021-06-05 18:27:07 -0700

    refactor toolbar

--- src/components/embed.tsx
+++ src/components/embed.tsx
@@ -3,8 +3,7 @@ import Excalidraw from "@excalidraw/excalidraw";
 import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/components/App";
 import quip from "quip-apps-api";
 import { RootEntity } from "../model/root";
-import { getData } from './utils'
-
+import { createToolbar } from '../utils/toolbar'
 
 import type { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
 import { ImportedDataState } from "@excalidraw/excalidraw/types/data/types";
@@ -44,56 +43,6 @@ export default function Embed(props: Props) {
         viewMode = true;
     }
 
-    const importFile = () => {
-        quip.apps.showFilePicker(
-            () => console.log('files picked'),
-            blobWithThumbnails => {
-                const blob = blobWithThumbnails[0].blob;
-                const filename = blob.getFilename();
-                // TODO: check filename finishes with .excalidraw
-                blob.onDataLoaded(blob => {
-                    const data = blob.getData();
-                    const string = new TextDecoder().decode(data);
-                    const parsed = JSON.parse(string);
-                    const elements = parsed.elements;
-                    const appState = parsed.appState;
-
-                    props.rootRecord.set('elements', JSON.stringify(elements));
-                    props.rootRecord.set('state', JSON.stringify(appState));
-
-                    excalidrawRef.current!.updateScene({ elements, appState});
-                    excalidrawRef.current!.setScrollToContent(elements);
-                });
-            },
-        );
-        // I don't know why we need to return bool. 
-        // Maybe is for error handling
-        return true;
-
-    }
-
-    const exportToConsole = () => {
-        const data = getData(props.rootRecord);
-        if (data === null) {
-            console.log('No data to download');
-            return false;
-        }
-        const fileInfo = {
-            "type": "excalidraw",
-            "version": 2,
-            "source": "https://excalidraw.com",
-            ...data
-        }
-        console.log(JSON.stringify(fileInfo));
-        console.log(
-            'For editing, copy last message into an editor, ' +
-            'save it with a .excalidraw extension. Then you ' +
-            'can upload it to excalidraw.com as a normal export file');
-        return true;
-    }
-
-
-
     useEffect(() => {
         const onHashChange = () => {
             const hash = new URLSearchParams(window.location.hash.slice(1));
@@ -104,30 +53,9 @@ export default function Embed(props: Props) {
         };
         // TODO: This shouldn't be here, but I also need the excalidrawRef
         // First option is to create the toolbar in root.ts and update the handler function at runtime
-        // Second option is to create the excalidrawRef also in root.ts
-        quip.apps.updateToolbar({
-            menuCommands: [
-                {
-                    id: 'Import from file',
-                    label: "Import from Excalidraw file",
-                    handler: importFile,
-                    // Tried to receive the functions directly using this action
-                    // Couldn't figure out how to obtain the blob
-                    // actionId: quip.apps.DocumentMenuActions.SHOW_FILE_PICKER,
-                    // actionStarted: () => { console.log('click on immport button'    ) },
-                },
-                {
-                    id: 'Export to console',
-                    label: "Export to console",
-                    handler: exportToConsole,
-                    // Tried to receive the functions directly using this action
-                    // Couldn't figure out how to obtain the blob
-                    // actionId: quip.apps.DocumentMenuActions.SHOW_FILE_PICKER,
-                    // actionStarted: () => { console.log('click on immport button'    ) },
-                },
-            ],
-            toolbarCommandIds: ['Import from file', 'Export to console'],
-        });
+        // Second option is to create the excalidrawRef also in root.ts        
+        createToolbar(props.rootRecord, excalidrawRef)
+
         window.addEventListener("hashchange", onHashChange, false);
         return () => {
             window.removeEventListener("hashchange", onHashChange);
---
+++ src/utils/toolbar.tsx
@@ -0,0 +1,87 @@
+import quip from "quip-apps-api";
+import type { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/components/App";
+import { RootEntity } from "../model/root";
+import { getData } from '../components/utils'
+
+
+export const createToolbar = (
+    rootRecord: RootEntity,
+    excalidrawRef: React.RefObject<ExcalidrawImperativeAPI>
+) => quip.apps.updateToolbar({
+    menuCommands: [
+        {
+            id: 'Import from file',
+            label: "Import from Excalidraw file",
+            handler: () => importFile(rootRecord, excalidrawRef),
+            // Tried to receive the functions directly using this action
+            // Couldn't figure out how to obtain the blob
+            // actionId: quip.apps.DocumentMenuActions.SHOW_FILE_PICKER,
+            // actionStarted: () => { console.log('click on immport button'    ) },
+        },
+        {
+            id: 'Export to console',
+            label: "Export to console",
+            handler: () => exportToConsole(rootRecord),
+            // Tried to receive the functions directly using this action
+            // Couldn't figure out how to obtain the blob
+            // actionId: quip.apps.DocumentMenuActions.SHOW_FILE_PICKER,
+            // actionStarted: () => { console.log('click on immport button'    ) },
+        },
+    ],
+    toolbarCommandIds: ['Import from file', 'Export to console'],
+});
+
+
+
+export const exportToConsole = (rootRecord: RootEntity) => {
+    console.log('woooo');
+    const data = getData(rootRecord);
+    if (data === null) {
+        console.log('No data to download');
+        return false;
+    }
+    const fileInfo = {
+        "type": "excalidraw",
+        "version": 2,
+        "source": "https://excalidraw.com",
+        ...data
+    }
+    console.log(JSON.stringify(fileInfo));
+    console.log(
+        'For editing, copy last message into an editor, ' +
+        'save it with a .excalidraw extension. Then you ' +
+        'can upload it to excalidraw.com as a normal export file');
+    return true;
+}
+
+
+export const importFile = (
+    rootRecord: RootEntity,
+    excalidrawRef: React.RefObject<ExcalidrawImperativeAPI>
+) => {
+    quip.apps.showFilePicker(
+        () => console.log('files picked'),
+        blobWithThumbnails => {
+            const blob = blobWithThumbnails[0].blob;
+            const filename = blob.getFilename();
+            // TODO: check filename finishes with .excalidraw
+            blob.onDataLoaded(blob => {
+                const data = blob.getData();
+                const string = new TextDecoder().decode(data);
+                const parsed = JSON.parse(string);
+                const elements = parsed.elements;
+                const appState = parsed.appState;
+
+                rootRecord.set('elements', JSON.stringify(elements));
+                rootRecord.set('state', JSON.stringify(appState));
+
+                excalidrawRef.current!.updateScene({ elements, appState });
+                excalidrawRef.current!.setScrollToContent(elements);
+            });
+        },
+    );
+    // I don't know why we need to return bool. 
+    // Maybe is for error handling
+    return true;
+
+}
